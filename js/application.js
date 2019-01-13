a1d148cce16c6a9373e0a00af8f85c8ffc611380
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-13 15:44:41 +0800

    added jquery total

--- js/application.js
+++ js/application.js
@@ -1,25 +1,34 @@
 
-var quantityCost = function() {
-  $('tbody tr').each(function (ind, el) {
-
+var quantityCost = function(el) {
     var price = parseFloat($(el).find('.price').text());
     var quantity = parseInt($(el).find('.quantity input').val());
     var cost = price * quantity;
 
     $(el).children('.cost').html(cost);
-
     return cost;
-  });
 };
+
+var updateList = function() {
+  var total = [];
+
+  $('tbody tr').each(function(ind, el) {
+    var totalCost = quantityCost(el);
+    total.push(totalCost)
+  });
+  total = total.reduce(function(a,b) {
+    return a + b;
+  })
+  $('#total').html(total);
+  };
+
 
 $(document).ready(function () {
   // var item = $(el).children('.item').text();
   $('tr:odd').css("background-color", "#e8e8e8");
   var total = [];
-
   $('.btn .remove').on('click', function(event) {
     $(this).closest('tr').remove();
-
   });
-  quantityCost()
+  quantityCost();
+  updateList();
 });
