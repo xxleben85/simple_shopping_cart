a630d172e82b13a4d669c284c1080538bf3ab524
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-13 12:26:35 +0800

    creating variables for jquery

--- js/application.js
+++ js/application.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   $('tr:odd').css("background-color", "#e8e8e8");
 
   $('tbody tr').each(function (ind, el) {
-    var item = $(el).children.first.text();
-    alert(item)
+    var item = $(el).children('.item').text();
+
+    var price = parseFloat($(el).children('.price').text());
+    var quantity = parseFloat($(el).children('.quantity').text());
+		var cost = price * quantity;
+    console.log(cost)
   })
 
 
